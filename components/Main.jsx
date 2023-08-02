efb0dd991e1368798089dcb392e496c3fe23ac80
Dmigdol <Dillonmigdol@gmail.com>
2023-08-02 12:20:01 -0700

    End of day 1
     Please enter the commit message for your changes. Lines starting

---
+++ components/Main.jsx
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+import Landing from './Pages/Landing'
+function Main() {
+
+  return (
+    <div id="Main" className="bg-slate-600 flex h-screen">
+      <Landing />
+    </div>
+  )
+
+}
+
+export default Main;
---
+++ components/Pages/Landing.jsx
@@ -0,0 +1,45 @@
+import React, {useState, useEffect} from 'react';
+import Image from 'next/image'
+
+/* IDEAL LANDING:
+    Contains an IMG, Job Title, and Name
+      Under the Name include a small description
+      Under IMG include buttons to socials
+      _____________________________________
+              Tech Stack Carousel
+      _____________________________________
+*/
+
+function Landing() {
+  return (
+  <div className="Landing-Base border-2 mx-24 inline-flex relative h-1/3 w-screen border-yellow-400 mt-14">
+    <div className="border-2 h-2/3 w-1/3">
+    <Image
+      src="/d1.png"
+      width={500}
+      height={500}
+    />
+    <div className="">
+    <Image
+    src='/github-logo.png'
+    width={45}
+    height={45}
+    />
+    </div>
+    </div>
+    <div className="Text-Landing border-2 h-full w-2/3 ">
+      <p className='text-3xl text-center pt-8'>Software Engineer</p>
+      <p className='text-4xl text-center pt-2'>Dillon Migdol</p>
+      <p className='text-lg text-ri pl-5 pt-1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+    </div>
+  </div>
+  )
+}
+
+// Necessary Styles
+
+const dillStyle = {
+
+}
+
+export default Landing;
