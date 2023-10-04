c71dc1f9f46ef6dfe6e25646938659c67e086c3d
Dmigdol <Dillonmigdol@gmail.com>
2023-10-04 14:30:41 -0700

    Adjusted css regarding summaries

--- components/Pages/Paragraphs/Paragraphs.jsx
+++ components/Pages/Paragraphs/Paragraphs.jsx
@@ -4,7 +4,7 @@ function abtMe() {
   return (
     <div>
       {Object.entries(Pinfo).map((item) => (
-        <h1 className='paragraphs'>
+        <h1 className='paragraphs border-b-4 border-orange-400'>
         <p className={`text-3xl pt-8 ${item[0] === 'Experience' ? 'text-right pr-9': 'pl-9'}`}>{item[0]}</p>
         <p className='m-8 text-xl'>{item[1]}</p>
       </h1>
