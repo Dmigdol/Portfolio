7d1e8eb585d24edf6bbd9bf33693aeae3a5228fa
Dmigdol <Dillonmigdol@gmail.com>
2023-10-08 12:10:48 -0700

    Updated Experience paragraph

--- components/Pages/Paragraphs/Pinfo.js
+++ components/Pages/Paragraphs/Pinfo.js
@@ -1,5 +1,12 @@
 let Pinfo = {
-  'About Me': 'Growing up in Southern California, I have walked many paths and aquired many hobbies. Always up for trying new things, many many many MANY years ago I found a love for technology. Around the age of 7 or 8, some placed a Nintendo GameCube controller in my hand and booted up Metroid Prime and I was hooked. Fast-forwarding many years I had established an identity with technology, building my own PC and fixing basic appliances. I truly can not see a future without these keystones in my life.',
-  'Experience': 'Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiusmod temporincididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitationullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptatevelit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident,sunt in culpa qui officia deserunt mollit anim id estlaborum.'
+  'About Me': `Growing up in Southern California, I have walked many paths and aquired many hobbies.
+    Always up for trying new things, many many many MANY years ago I found a love for technology. Around the age of 7 or 8, some placed a Nintendo GameCube controller in my hand and booted up Metroid Prime and I was hooked.
+    Fast-forwarding many years I had established an identity with technology, building my own PC and fixing basic appliances. I truly can not see a future without these keystones in my life.`,
+
+  'Experience': `Beginning my Software enginneering journey at College of the Canyons, I spent around 2-3 years working a full time job while squeezing classes in when I could.
+    The extremely slow pace at which I was progressing in school, paired with the Covid-19 Pandemic that basically shut me out of a year of my school career, lead me to putting my bachelors on hold and enrolling in Hack Reactor.
+    This gave me sufficient experience using Javascript to be qualified as a FullStack engineer.
+    I have been a Project Manager for a team of 8 software engineers, creating an end to end phone application in React native.
+    Along with this, I have had multiple key roles developing the front and back of various mock E-commerce sites.`
 }
 export default Pinfo;
