38ddea88b1973d7cf23abab5a9266043ef52635b
Dmigdol <Dillonmigdol@gmail.com>
2023-10-15 12:25:59 -0700

    Fixed css for rendering the video players, considering using Modals

---
+++ components/Pages/Projects/Pjinfo.js
@@ -0,0 +1,7 @@
+let PjInfo = {
+  'Test Title' : 'https://www.youtube.com/watch?v=_gF21Jimxi0',
+  'Test 2' : 'https://www.youtube.com/watch?v=_gF21Jimxi0',
+
+}
+
+export default PjInfo;
--- components/Pages/Projects/Projects.jsx
+++ components/Pages/Projects/Projects.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactPlayer from 'react-player/lazy'
+import PjInfo from './Pjinfo.js'
+
 function Projects() {
 
   return (
@@ -7,11 +9,22 @@ function Projects() {
       <header className='mt-4 paragraphs text-3xl'>
         Projects
       </header>
-      <vid>
-        <ReactPlayer
-        url='https://www.youtube.com/watch?v=_gF21Jimxi0'/>
-      </vid>
+      <div className='flex'>
+      {Object.entries(PjInfo).map((item) => (
+        <h1 className='paragraphs w-screen'>
+          <div>
+            <p className='m-5 text-2xl'>{item[0]}</p>
+            <vid className='flex justify-center'>
+              <ReactPlayer
+              url={item[1]}
+              height='180px'
+              width='320px'/>
+            </vid>
+        </div>
+      </h1>
+      ))}
     </div>
+  </div>
   )
 }
 export default Projects;
