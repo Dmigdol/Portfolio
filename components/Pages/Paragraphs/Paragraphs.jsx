d033646d15cbb4afc1105f6122b8028a12fca82f
Dmigdol <Dillonmigdol@gmail.com>
2023-12-20 11:59:00 -0800

    Changes to formatting regarding paragraphs

--- components/Pages/Paragraphs/Paragraphs.jsx
+++ components/Pages/Paragraphs/Paragraphs.jsx
@@ -4,19 +4,26 @@ function abtMe() {
   return (
     <div>
       {Object.entries(Pinfo).map((item) => (
-        <h1 className='paragraphs border-b-4 border-orange-400'>
-        <div className='text-3xl pt-8'>
-          {item[0] === 'Experience' ?
-            <div className='text-right pr-9'>
-              {item[0]}
+        <h1 className='paragraphs border-b-4 flex border-orange-400'>
+        <div className='text-7xl pt-8 '>
+          <div className='tricontainer'>
+            {item[0] === 'Experience' ?
+            <div>
+              <div className='text-right w-1/3 h-max float-right border-2 pr-9'>
+                {item[0]}
+              </div>
+              <p className='m-8 text-2xl w-2/4'>{item[1]}</p>
             </div>
-          :
-            <div className='pl-9'>
-              {item[0]}
+            :
+            <div>
+              <div className='pl-9 border-2 w-1/4 float-left'>
+                {item[0]}
+              </div>
+              <p className='m-8 text-2xl w-2/4 float-right'>{item[1]}</p>
             </div>
-          }
+            }
+          </div>
         </div>
-        <p className='m-8 text-xl'>{item[1]}</p>
       </h1>
       ))}
     </div>
