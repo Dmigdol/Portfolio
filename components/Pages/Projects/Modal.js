859e52b33ff01c8e1252f7888382584b59c13908
Dmigdol <Dillonmigdol@gmail.com>
2024-01-07 14:57:04 -0800

    Added a description for The World

--- components/Pages/Projects/Modal.js
+++ components/Pages/Projects/Modal.js
@@ -13,7 +13,7 @@ function Modal({closeModal, currentModal}) {
   w-5/6 h-5/6
   '>
     <div className='modalContainer'>
-      <div className='title paragraphs pb-8 mt-5 text-center text-5xl'>Title</div>
+      <div className='title paragraphs pb-8 mt-5 text-center text-5xl'>{currentModal[1].Title}</div>
       <div className='closeBtn absolute right-0 top-0 text-golden text-4xl'>
         <button
         onClick={() => {closeModal(false); document.body.style.overflow = "visible"
@@ -30,7 +30,7 @@ function Modal({closeModal, currentModal}) {
       </div>
       <div className='border-2 w-3/5 mx-auto mt-7'>
         <p className='body m-0 break-words'>
-          Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah
+          {currentModal[1].description}
         </p>
       </div>
       <span className='Stack absolute float-left left-8 bottom-9 text-xl'>Tech Stack : </span>
--- components/Pages/Projects/Pjinfo.js
+++ components/Pages/Projects/Pjinfo.js
@@ -1,7 +1,9 @@
 let PjInfo = {
   'The World' : {
+    Title: 'The World',
     videoUrl : 'https://www.youtube.com/watch?v=_gF21Jimxi0',
-    githubUrl : 'https://github.com/JoJos-Blue-Ocean/TheWorld'
+    githubUrl : 'https://github.com/JoJos-Blue-Ocean/TheWorld',
+    description : 'The World is a vinyl record exchange app. The target audience for this app is attracting Vinyl enthusiasts, younger collectors, and trading communities. It is an app where users can exchange vinyls and set up a meeting through the platform.'
   },
   'test' : {
     videoUrl: 'N/A',
